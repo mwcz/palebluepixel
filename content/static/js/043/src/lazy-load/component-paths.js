83ccedc49952d5612f0d6673c009cedb643514ff
mwcz <mwc@clayto.com>
2016-09-08 14:57:43 -0400

    add lazyloading web components draft

---
+++ content/static/js/043/src/lazy-load/component-paths.js
@@ -0,0 +1,8 @@
+var LazyComponents = {
+    names: 'bs-panel,bs-btn,bs-alert',
+    paths: {
+        'bs-panel': './src/bs-components/bs-panel/bs-panel.html',
+        'bs-btn': './src/bs-components/bs-btn/bs-btn.html',
+        'bs-alert': './src/bs-components/bs-alert/bs-alert.html',
+    },
+};
