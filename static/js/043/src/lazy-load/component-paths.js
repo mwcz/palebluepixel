824b0d585d0cf6a74c210533e9f294cde20a9823
mwcz <mwc@clayto.com>
2017-02-08 17:20:41 -0500

    Generate Pelican site

--- static/js/043/src/lazy-load/component-paths.js
+++
@@ -1,8 +0,0 @@
-var LazyComponents = {
-    names: 'ws-panel,ws-btn,ws-alert',
-    paths: {
-        'ws-panel': './src/ws-components/ws-panel/ws-panel.html',
-        'ws-btn': './src/ws-components/ws-btn/ws-btn.html',
-        'ws-alert': './src/ws-components/ws-alert/ws-alert.html',
-    },
-};
